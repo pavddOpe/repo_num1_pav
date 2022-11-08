eced03391791f8346ea2fc41ebc53f7f5b6d6aa0
bAvOvniatk0 <116340670+bAvOvniatk0@users.noreply.github.com>
2022-11-08 13:38:36 +0100

    this_commit

--- pages/login_page.py
+++ pages/login_page.py
@@ -3,8 +3,9 @@
 #
 # class LoginPage(BasePage):
 #     login_field_xpath = "//*[@id='login']"
-#     password_field_xpath =
-#     sign_in_button_xpath =
-#
+#     password_field_xpath ="//*[@id='user_password']"
+#     sign_in_button_xpath ="//*[id='signinButton']"
+#     email = 'pashadenysyuk0@gmail.com'
+
 #     def type_in_email(self, email):
 #         self.field_send_keys(self.login_field_xpath, email)
