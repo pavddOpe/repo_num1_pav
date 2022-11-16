e10975e9963e924dcef4360c2d5064cb7e10335a
bAvOvniatk0 <116340670+bAvOvniatk0@users.noreply.github.com>
2022-11-16 19:37:18 +0100

    again_commit

--- pages/adding_match.py
+++ pages/adding_match.py
@@ -2,13 +2,13 @@ from pages.base_page import BasePage
 
 
 class AddingMatch(BasePage):
-    my_team_xpath = "//*[@id='__next']/div[1]/main/div[2]/form/div[2]/div/div[1]/div/div/input"
-    enemy_team_xpath = "/html/body/div/div[1]/main/div[2]/form/div[2]/div/div[2]/div/div/input"
+    my_team_xpath = "//div[1]/div/div/input"
+    enemy_team_xpath = "//div[2]/div/div/input"
     my_team_score_xpath = "//input[contains(@name,'myTeamScore')]"
     enemy_team_score_xpath = "//input[contains(@name,'enemyTeamScore')]"
-    date_xpath = "//*[@id='__next']/div[1]/main/div[2]/form/div[2]/div/div[5]/div/div/input"
+    date_xpath = "//div[5]/div/div/input"
     league_xpath = "//input[starts-with(@name,'league')]"
-    time_played_xpath = "//html/body/div/div[1]/main/div[2]/form/div[2]/div/div[9]/div/div/input"
-    rating_xpath = "//*[@id='__next']/div[1]/main/div[2]/form/div[2]/div/div[13]/div/div/input"
+    time_played_xpath = "//div[9]/div/div/input"
+    rating_xpath = "//div[13]/div/div/input"
     submit_button_xpath = "//span[text()='Submit']"
     clear_button_xpath = "//span[text()='Clear']"
